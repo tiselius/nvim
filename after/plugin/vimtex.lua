f004f87b9e40d331cd1656af06ffc7783ffa34cf
Aron Tiselius <aron.tiselius@gmail.com>
2025-04-29 15:28:24 +0200

    changed pdf viewer for latex

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -4,6 +4,7 @@ vim.cmd("filetype plugin indent on")
 -- This enables Vim's and Neovim's syntax-related features.
 vim.cmd("syntax enable")
 
+vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_view_skim_activate = 1
 vim.g.vimtex_view_skim_reading_bar = 1
 
